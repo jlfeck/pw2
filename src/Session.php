cb710d16091efd8f4055b6eb64587aaf7045575b
João Feck Jr <jl.feck@gmail.com>
2014-11-21 18:40:16 -0200

    isset

--- src/Session.php
+++ src/Session.php
@@ -7,9 +7,9 @@ class Session
 	public static function start($data = array())
 	{	
 		if (!empty($data)) {
-			$_SESSION['currentId'] = $data['currentId'];
-			$_SESSION['currentUser'] = $data['currentUser'];
-			$_SESSION['currentName'] = $data['currentName'];
+			$_SESSION['currentId'] = (isset($data['currentId'])) ? $data['currentId'] : '' ;
+			$_SESSION['currentUser'] = (isset($data['currentUser'])) ? $data['currentUser'] : '' ;
+			$_SESSION['currentName'] = (isset($data['currentName'])) ? $data['currentName'] : '' ;
 		} else {
 			session_unset();
 		}
